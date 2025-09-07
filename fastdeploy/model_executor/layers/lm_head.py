4663a22814ffc66927aa5c3006180656e9cb584b
lizhenyun01 <1500424927@qq.com>
2025-09-07 16:39:58 +0800

    fix bias rl_tp_degree

--- fastdeploy/model_executor/layers/lm_head.py
+++ fastdeploy/model_executor/layers/lm_head.py
@@ -94,7 +94,8 @@ class ParallelLMHead(nn.Layer):
                         "model_format": self.fd_config.model_config.model_format,
                     },
                 )
-                self.linear.bias.rl_tp_degree = fd_config.parallel_config.tensor_parallel_size
+                if self.bias_key is not None:
+                    self.linear.bias.rl_tp_degree = fd_config.parallel_config.tensor_parallel_size
                 if self.nranks > 1:
                     set_weight_attrs(self.linear.weight, {"output_dim": True})
             else:
