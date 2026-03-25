89637d9ba5d7dcb11500588672b99d782081059c
lizhenyun01 <1500424927@qq.com>
2026-03-25 18:17:05 +0800

    fix

--- fastdeploy/spec_decode/mtp.py
+++ fastdeploy/spec_decode/mtp.py
@@ -379,6 +379,9 @@ class MTPProposer(Proposer):
         self.model_inputs["kv_num_blocks_x_cpu"] = paddle.zeros_like(
             self.target_model_inputs["kv_num_blocks_x_cpu"]
         ).cpu()
+        self.model_inputs["tmp_workspace"] = paddle.zeros_like(self.target_model_inputs["tmp_workspace"])
+        self.model_inputs["tmp_m"] = paddle.zeros_like(self.target_model_inputs["tmp_m"])
+        self.model_inputs["tmp_d"] = paddle.zeros_like(self.target_model_inputs["tmp_d"])
 
         # Get the attention backend
         attn_cls = get_attention_backend()
@@ -903,6 +906,9 @@ class MTPProposer(Proposer):
             kv_batch_ids=self.model_inputs["kv_batch_ids"],
             kv_tile_ids_per_batch=self.model_inputs["kv_tile_ids_per_batch"],
             kv_num_blocks_x_cpu=self.model_inputs["kv_num_blocks_x_cpu"],
+            tmp_workspace=self.model_inputs["tmp_workspace"],
+            tmp_m=self.model_inputs["tmp_m"],
+            tmp_d=self.model_inputs["tmp_d"],
             attn_mask_offsets=self.model_inputs["attn_mask_offsets"] if self.enable_mm else None,
         )
 
